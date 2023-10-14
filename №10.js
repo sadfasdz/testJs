b3423498f9d7305958e7e0012877603702f69bf2
A A <sagdshfh@mail.ru>
2023-10-15 01:48:16 +0300

    exercise 19

--- №10.js
+++ №10.js
@@ -1,72 +1,59 @@
-function convertStringToJson(str) {
-  let result = null;
-  if (typeof str === "string") {
-    str = str.trim();
-    if (str[0] === "{" && str[str.length - 1] === "}") {
-      str = str.slice(1, -1);
-      result = {};
-      let keyValuePairs = str.split(",");
-      for (let i = 0; i < keyValuePairs.length; i++) {
-        let [key, value] = keyValuePairs[i].split(":");
-        key = key.trim().replace(/"/g, "");
-        value = value.trim();
-        if (value[0] === "{") {
-          value = convertStringToJson(value);
-        } else if (value[0] === "[") {
-          value = convertStringToArray(value);
-        } else if (value[0] === '"' && value[value.length - 1] === '"') {
-          value = value.slice(1, -1);
-        } else if (value === "true") {
-          value = true;
-        } else if (value === "false") {
-          value = false;
-        } else if (value === "null") {
-          value = null;
-        } else if (!isNaN(value)) {
-          value = Number(value);
-        }
-        result[key] = value;
-      }
-    } else if (str[0] === "[" && str[str.length - 1] === "]") {
-      result = convertStringToArray(str);
-    }
+function convertToJson(string) {
+  // проверяем, что строка не пустая и не равна null
+  if (!string || string === "null") {
+    return null;
   }
-  return result;
+
+  // удаляем пробелы в начале и конце строки
+  string = string.trim();
+
+  // проверяем, что строка начинается с '{' и заканчивается '}'
+  if (string[0] !== "{" || string[string.length - 1] !== "}") {
+    throw new Error("Invalid JSON string");
+  }
+
+  // удаляем '{' и '}' из строки
+  string = string.slice(1, -1);
+
+  // разбиваем строку на массив пар ключ-значение
+  const pairs = string.split(",");
+
+  // создаем объект для хранения пар ключ-значение
+  const obj = {};
+
+  // проходимся по каждой паре ключ-значение
+  for (let pair of pairs) {
+    // разбиваем пару на ключ и значение
+    const [key, value] = pair.split(":");
+
+    // удаляем пробелы в начале и конце ключа и значения
+    const cleanKey = key.trim();
+    const cleanValue = value.trim();
+
+    // проверяем, что ключ начинается и заканчивается кавычками
+    if (cleanKey[0] !== '"' || cleanKey[cleanKey.length - 1] !== '"') {
+      throw new Error("Invalid JSON string");
+    }
+
+    // удаляем кавычки из ключа
+    const keyWithoutQuotes = cleanKey.slice(1, -1);
+
+    // проверяем, что значение начинается и заканчивается  кавычками
+    if (cleanValue[0] !== '"' || cleanValue[cleanValue.length - 1] !== '"') {
+      throw new Error("Invalid JSON string");
+    }
+
+    // удаляем кавычки из значения
+    const valueWithoutQuotes = cleanValue.slice(1, -1);
+
+    // добавляем пару ключ-значение в объект
+    obj[keyWithoutQuotes] = isNaN(valueWithoutQuotes)
+      ? valueWithoutQuotes
+      : Number(valueWithoutQuotes);
+  }
+
+  return obj;
 }
 
-function convertStringToArray(str) {
-  let result = [];
-  if (
-    typeof str === "string" &&
-    str[0] === "[" &&
-    str[str.length - 1] === "]"
-  ) {
-    str = str.slice(1, -1);
-    let values = str.split(",");
-    for (let i = 0; i < values.length; i++) {
-      let value = values[i].trim();
-      if (value[0] === "{") {
-        value = convertStringToJson(value);
-      } else if (value[0] === "[") {
-        value = convertStringToArray(value);
-      } else if (value[0] === '"' && value[value.length - 1] === '"') {
-        value = value.slice(1, -1);
-      } else if (value === "true") {
-        value = true;
-      } else if (value === "false") {
-        value = false;
-      } else if (value === "null") {
-        value = null;
-      } else if (!isNaN(value)) {
-        value = Number(value);
-      }
-      result.push(value);
-    }
-  }
-  return result;
-}
-
-const jsonString =
-  '{"name":"John","age":30,"city":"New York","isMarried":true,"hobbies":["reading","swimming"],"address":{"street":"Main St","number":100}}';
-const json = convertStringToJson(jsonString);
-console.log(json);
+const jsonString = '{"name": "John", "age": "30"}';
+console.log(convertToJson(jsonString));
--- №19/app.js
+++ №19/app.js
@@ -5,32 +5,72 @@ const owner_id = "59120170";
 
 const url = `https://api.vk.com/method/wall.get?access_token=${access_token}&owner_id=-${owner_id}&count=100&v=5.154`;
 
-const getPosts = async function (url) {
+async function getPosts(url) {
   const response = await fetch(url);
   const data = await response.json();
   showPosts(data);
-  savePosts(data);
+  savePosts();
+
   return data;
-};
+}
 
 function showPosts(posts) {
   const list = document.querySelector(".posts");
   console.log(posts);
+  console.log(posts.response.items[1].date);
   for (let i = 1; i < posts.response.items.length; i++) {
     const li = document.createElement("li");
-    // const time = new Date().getTime();
-    // console.log(time);
-    li.innerHTML = `<div class="post-text">${posts.response.items[i].text}</div>
-                  <div class="post-date">${new Date(
-                    posts.response.items[i].date
-                  ).toString()}</div>`;
-
-    // img.src = posts.response.items[1].attachments[0].photo.sizes[2].url;
-
+    li.classList.add("posts-item");
+    li.innerHTML = `${
+      posts.response.items[i].attachments[0]?.photo?.sizes[7]?.url
+        ? `<img src="${posts.response.items[i].attachments[0]?.photo?.sizes[7].url}"> `
+        : "<div>no img</div>"
+    }
+     
+    <div class="post-text">Текст поста: ${posts.response.items[i].text}</div>
+                  <div class="post-date">Дата поста: ${new Date(
+                    posts.response.items[i].date * 1000
+                  ).toLocaleDateString()}</div>`;
     list.append(li);
   }
 }
-
-function savePosts(posts) {}
-
 getPosts(url);
+
+const arrPosts = [];
+function savePosts() {
+  let callback = (entries, observer) => {
+    const post = {};
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        console.log(entry);
+        post.img = entry.target.children[0].currentSrc;
+        post.text = entry.target.children[1].innerText;
+        post.date = entry.target.children[2].innerText;
+        arrPosts.push(post);
+        try {
+          localStorage.setItem("posts", JSON.stringify(arrPosts));
+        } catch {
+          const posts = JSON.parse(localStorage.getItem("posts"));
+          posts.shift();
+          localStorage.setItem("posts", JSON.stringify(arrPosts));
+        }
+
+        observer.unobserve(entry.target);
+      }
+    });
+  };
+
+  let options = {
+    root: document.querySelector(".posts"),
+    rootMargin: "0px",
+    threshold: 1.0,
+  };
+
+  let observer = new IntersectionObserver(callback, options);
+
+  let postItems = document.querySelectorAll(".posts-item");
+
+  postItems.forEach((target) => {
+    observer.observe(target);
+  });
+}
